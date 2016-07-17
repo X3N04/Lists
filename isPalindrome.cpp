40f488db956e9975798319c2402a17bad5c42e07
Daniel <dsalazar10@horizon.csueastbay.edu>
2016-07-17 16:57:51 -0700

    Update isPalindrome.cpp
    Off by one error in line 8 fixed.

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -5,7 +5,7 @@ bool isPalindrome(LinkedList<char> &s)
 	if (s.isEmpty() || s.getLength() == 1)
 		return true;
 	else if (s.getEntry(1) == s.getEntry(s.getLength()))
-		return isPalindrome(s.subStr(2, s.getLength() - 1));
+		return isPalindrome(s.subStr(2, s.getLength() - 2));
 	else
 		return false;
 }
@@ -13,10 +13,10 @@ int main()
 {
 	LinkedList<char> word1, word2;
 	char s1[] = { "tattattat" };
-	for (unsigned int i = 0; i < strlen(s1); ++i) word1.insert(s1[i]);
+	for (unsigned int i = 0; i < strlen(s1); ++i) word1.push(s1[i]);
 	std::cout << isPalindrome(word1) << std::endl;
 	char s2[] = { "ermehgerd" };
-	for (unsigned int i = 0; i < strlen(s2); ++i) word2.insert(s2[i]);
+	for (unsigned int i = 0; i < strlen(s2); ++i) word2.push(s2[i]);
 	std::cout << isPalindrome(word2) << std::endl;
 	int stop;
 	std::cin >> stop;
